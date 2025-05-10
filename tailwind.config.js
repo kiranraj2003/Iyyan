2abec18445b20b25733ddf61bb7cc83827676f4a
kiranraj2003 <kiran25042003@gmail.com>
2025-05-10 23:38:32 +0530

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+       boxShadow: {
+        'custom': '0 4px 8px rgba(0, 0, 0, 0.1)', // custom-shadow definition
+      }
+    },
   },
   plugins: [],
 }
